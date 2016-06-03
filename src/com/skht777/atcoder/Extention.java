6412942e21b5617c0e7b8287f5af482df90fa83c
skht777 <skht@skht-laboratory.tech>
2016-06-03 16:40:06 +0900

    新しい提出可能言語に対応

--- src/com/skht777/atcoder/Extention.java
+++ src/com/skht777/atcoder/Extention.java
@@ -15,42 +15,61 @@ import javafx.stage.FileChooser.ExtensionFilter;
  */
 public enum Extention {
 	
+	AWK("Awk", "*.awk"),
 	BASH("Bash", "*.sh"),
+	BRAINFUCK("Brainfuck", "*.bf"),
 	C("C", "*.c"),
 	CPP("C++", "*.cpp"),
 	CSHARP("C#", "*.cs"),
+	CEYLON("Ceylon", "*.ceylon"),
 	CLOJURE("Clojure", "*.clj"),
 	COLISP("Common Lisp", "*.lisp"),
+	CRYSTAL("Crystal", "*.cr"),
+	FSHARP("F#", ".*fs", "*.fsx"),
+	FORTRAN("Fortran", "*.f08", "*.f", "*.for", "*.f90", "*.f95", "*.F", "*.F90", "*.F95"),
 	D("D", "*.d"),
 	GO("Go", "*.go"),
 	HASKELL("Haskell", "*.hs", "*.lhs"),
 	JAVA("Java", "*.java"),
 	JAVASCRIPT("JavaScript", "*.js"),
+	JULIA("Julia", "*.jl"),
+	KOTLIN("Kotlin", "*.kt"),
+	LUA("Lua", "*.lua"),
+	LUAJIT("LuaJIT", "*.lua"),
+	MOONSCRIPT("MoonScript", "*.moon"),
+	NIM("Nim", "*.nim"),
+	OBJECTIVE_C("Objective-C", "*.m"),
 	OCAML("OCaml", "*.ml"),
+	OCTAVE("Octave", "*.m"),
 	PASCAL("Pascal", "*.pas"),
 	PERL("Perl", "*.pl"),
 	PHP("PHP", "*.php"),
 	PYTHON("Python", "*.py"),
+	PYPY("PyPy", "*.py"),
 	RUBY("Ruby", "*.rb"),
+	RUST("Rust", "*.rs"),
 	SCALA("Scala", "*.scala"),
 	SCHEME("Scheme", "*.scm"),
-	TEXT("Text", "*.txt");
+	SED("Sed", "*.sed"),
+	STDML("Standard ML", "*.ml"),
+	SWIFT("Swift", "*.swift"),
+	TEXT("Text", "*.txt"),
+	TYPESCRIPT("TypeScript", "*.ts"),
+	UNLAMBDA("Unlambda", "*.unl"),
+	VB("Visual Basic", "*.vb");
 	
-	private String name;
-	private String[] extention;
+	private ExtensionFilter filter;
 	
 	private Extention(String name, String... extention) {
-		this.name = name;
-		this.extention = extention;
+		filter = new ExtensionFilter("name", extention);
 	}
 	
 	public static List<ExtensionFilter> getExtensionList() {
-		return Arrays.stream(Extention.values()).map(e->new ExtensionFilter(e.name, e.extention)).collect(Collectors.toList());
+		return Arrays.stream(Extention.values()).map(e->e.filter).collect(Collectors.toList());
 	}
 	
 	public static ExtensionFilter toExtensionFilter(String name) {
-		Extention e = Arrays.stream(Extention.values())
-				.filter(ex->ex.name.equals(name.replaceFirst("\\d{0,2} \\(.+\\)", ""))).findFirst().orElse(Extention.TEXT);
-		return new ExtensionFilter(e.name, e.extention);
+		return Arrays.stream(Extention.values()).filter(ex->ex.filter.getDescription().equalsIgnoreCase(name.replaceFirst("\\d{0,2} \\(.+\\)", "")))
+				.findFirst().orElse(Extention.TEXT).filter;
 	}
 }
